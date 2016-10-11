09593e9e9ff3921d7cda8a8a8f7223cc11775865
byzhkc3 <byzhkc3@gmail.com>
2016-10-11 14:07:08 +0800

    去除无关内容

--- myAjax.js
+++ myAjax.js
@@ -6,10 +6,7 @@
 // fun2 失败后执行的方法
 
 function myAjax (url, data, method, func1, func2) {
-	if (dummy_data (url)!==null) {
-		console.log ('got dummy data for ', url, data);
-		return func1(dummy_data (url));
-	}
+	
 	var handle_error = function (jqXHR, textStatus) {
 		// ajax 调用出错处理
 		if(jqXHR.state() == "resolved"){
